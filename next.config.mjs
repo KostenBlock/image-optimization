07275024ad101fd1787115b584fe74668fd980b0
kosteblock <kostenkosta@gmail.com>
2024-04-14 02:08:21 +0300

    first

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
-  trailingSlash: true,
   images: {
     // loader: 'custom',
     // loaderFile: "./loader.js",
     domains: ["storage.yandexcloud.net"],
     deviceSizes: [800, 1000, 1500, 2000, 2500, 4000],
-    unoptimized: true
   }
 };
 
